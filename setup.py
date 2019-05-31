51f9a2ce72453be3cdcb5ae679c094420670ae61
Daniel Dykiel <48263710+ddykiel@users.noreply.github.com>
2019-05-31 19:02:49 -0400

    Version for June 1st release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import sys
 from cx_Freeze import setup, Executable
 
-includefiles = ['Art/icon.png', 'Music/Anamalie.mp3', 'Music/Lone Harvest.mp3', 'Music/Morgana Rides.mp3', 'Music/Sovereign Quarter.mp3', 'Music/Evening of Chaos.mp3', "Art/MonstrousTitleScreen.png", "Credits.txt"]
+includefiles = ['Art/icon.png', 'Music/Anamalie.mp3', 'Music/Lone Harvest.mp3', 'Music/Morgana Rides.mp3', 'Music/Sovereign Quarter.mp3', 'Music/Evening of Chaos.mp3', "Art/MonstrousTitleScreen.png", "Credits.txt", "Art/Hand Monster.png", "Art/Teeth Monster.png", "Art/Eye Monster.png", "Art/Throat Monster.png"]
 includes = []
 packages = ['pygame']
 
